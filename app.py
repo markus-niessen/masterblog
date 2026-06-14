5bd5d138d4056676ffbd5d818cf7d03b46dfc410
Markus Nießen <markusniessen1@gmail.com>
2026-06-15 01:52:10 +0200

    Add blog post creation form and route

--- app.py
+++ app.py
@@ -1,17 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
+
 
 def load_posts():
     with open("blog_posts.json", "r") as file:
         return json.load(file)
 
-@app.route('/')
+
+def save_posts(posts):
+    with open("blog_posts.json", "w") as file:
+        json.dump(posts, file, indent=4)
+
+
+@app.route("/")
 def index():
-    posts = load_posts()
-    return render_template("index.html", posts=posts)
+    blog_posts = load_posts()
+    return render_template("index.html", posts=blog_posts)
 
 
-if __name__ == '__main__':
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        blog_posts = load_posts()
+
+        title = request.form.get("title")
+        author = request.form.get("author")
+        content = request.form.get("content")
+
+        new_id = max(post["id"] for post in blog_posts) + 1 if blog_posts else 1
+
+        new_post = {
+            "id": new_id,
+            "title": title,
+            "author": author,
+            "content": content
+        }
+
+        blog_posts.append(new_post)
+        save_posts(blog_posts)
+
+        return redirect(url_for("index"))
+
+    return render_template("add.html")
+
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
