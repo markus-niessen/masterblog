5565ed62eebe09dd03bc13ca1c6b687481c89377
Markus Nießen <markusniessen1@gmail.com>
2026-06-15 01:41:36 +0200

    Implement blog home page with JSON post storage

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
+def load_posts():
+    with open("blog_posts.json", "r") as file:
+        return json.load(file)
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    posts = load_posts()
+    return render_template("index.html", posts=posts)
 
 
 if __name__ == '__main__':
